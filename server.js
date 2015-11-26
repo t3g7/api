21b5a12edab57161457a1e1e90d3e10dca53a7d7
eftov <b.fovet@gmail.com>
2015-11-26 15:27:45 +0100

    Connect to cassandra and get tweets

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cassandra = require('cassandra-driver');
 
 var app = express();
+app.use(bodyParser.json());
+
+var client = new cassandra.Client({ contactPoints : ['127.0.0.1']});
+client.connect(function(err, res) {
+	console.log('Connected');
+});
 
 app.get('/', function(req, res) {
-	res.send("Hello World");
+	var getTweets = 'SELECT * FROM twitter_streaming.tweets;';
+	client.execute(getTweets, function(err, result) {
+		if (err) {
+			res.status(404).send({msg: 'Tweets not found'});
+		} else {
+			res.json(result);
+		}
+	});
 });
 
 app.listen(8080);
