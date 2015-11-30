a3a23afb29d1cf2844c7ad0a2c6a5e4114a3a488
eftov <b.fovet@gmail.com>
2015-11-30 08:57:44 +0100

    Enable CORS

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   // install middleware
   swaggerExpress.register(app);
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   app.get('/tweets', function(req, res) {
 	var getTweets = 'SELECT * FROM twitter_streaming.tweets;';
 	client.execute(getTweets, function(err, result) {
